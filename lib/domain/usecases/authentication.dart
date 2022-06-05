99e942f835ed99f1a14f90dec6998d7a5e2f46b8
Rafael Justo <rafael.justoscherer@gmail.com>
2022-06-05 17:48:35 -0300

    :construction: começando testes dos params do body

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -1,8 +1,17 @@
 import 'package:guess_the_music/domain/entities/account_entity.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth({
+  Future<AccountEntity> auth(AuthenticationParams params);
+}
+
+class AuthenticationParams {
+  final String email = '';
+  final String password = '';
+
+  AuthenticationParams({
     required String email,
     required String password,
   });
+
+  Map toJson() => {'email': email, 'password': password};
 }
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,4 +1,5 @@
 import 'package:faker/faker.dart';
+import 'package:guess_the_music/domain/usecases/authentication.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
@@ -7,25 +8,60 @@ class RemoteAuthentication {
   final String url;
 
   RemoteAuthentication({required this.httpClient, required this.url});
-  Future<void> auth() async {
-    await httpClient.request(url: url);
+
+  Future<void> auth(AuthenticationParams params) async {
+    await httpClient.request(url: url, method: 'post', body: params.toJson());
   }
 }
 
 abstract class HttpClient {
-  Future<void>? request({required String url});
+  Future<void>? request({
+    required String url,
+    String method = 'post',
+    Map? body,
+  });
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
+  RemoteAuthentication? sut;
+  String? url;
+  HttpClientSpy? httpClient;
+  AuthenticationParams? params;
+
+  setUp(() {
+    httpClient = HttpClientSpy();
+    url = faker.internet.httpUrl();
+    sut = RemoteAuthentication(httpClient: httpClient!, url: url!);
+    params = AuthenticationParams(
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    );
+  });
+
   test('Should call HttpClient with correct URL', () async {
-    final httpClient = HttpClientSpy();
-    final url = faker.internet.httpUrl();
-    final sut = RemoteAuthentication(httpClient: httpClient, url: url);
+    await sut!.auth(params!);
 
-    await sut.auth();
+    verify(httpClient!.request(url: url!, body: params!.toJson()));
+  });
 
-    verify(httpClient.request(url: url));
+  test('Should call HttpClient with correct method', () async {
+    await sut!.auth(params!);
+
+    verify(httpClient!.request(url: url!, method: 'post', body: params!.toJson()));
+  });
+
+  test('Should call HttpClient with correct body params', () async {
+    await sut!.auth(params!);
+
+    verify(httpClient!.request(
+      url: url!,
+      method: 'post',
+      body: {
+        'email': params?.email,
+        'password': params?.password,
+      },
+    ));
   });
 }
