523a14662dc6af0b6222d79796d371999239bbf4
Rafael Justo <rafael.justoscherer@gmail.com>
2022-06-07 18:59:30 -0300

    :construction: começando domainErrors e preparando testes para erros de output

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,9 +1,12 @@
 import 'package:faker/faker.dart';
-import 'package:guess_the_music/data/http/_http.dart';
-import 'package:guess_the_music/data/usecases/_usecases.dart';
-import 'package:guess_the_music/domain/usecases/authentication.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
+
+import 'package:guess_the_music/data/http/_http.dart';
+import 'package:guess_the_music/data/usecases/_usecases.dart';
+
+import 'package:guess_the_music/domain/helpers/_helpers.dart';
+import 'package:guess_the_music/domain/usecases/authentication.dart';
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
@@ -11,53 +14,41 @@ void main() {
   RemoteAuthentication? sut;
   String? url;
   HttpClientSpy? httpClient;
-  AuthenticationParams? params;
 
   setUp(() {
     httpClient = HttpClientSpy();
     url = faker.internet.httpUrl();
     sut = RemoteAuthentication(httpClient: httpClient!, url: url!);
-    params = AuthenticationParams(
+  });
+
+  test('Should call HttpClient with correct URL, method and params', () async {
+    final params = AuthenticationParams(
       email: faker.internet.email(),
       password: faker.internet.password(),
     );
-  });
 
-  test('Should call HttpClient with correct URL', () async {
-    await sut!.auth(params!);
+    await sut!.auth(params);
 
     verify(httpClient!.request(
       url: url!,
       body: {
-        'email': params?.email,
-        'password': params?.password,
+        'email': params.email,
+        'password': params.password,
       },
     ));
   });
 
-  test('Should call HttpClient with correct method', () async {
-    await sut!.auth(params!);
+  test('Should throw an unexpected exception if httpClient returns 400', () async {
+    when(httpClient!
+            .request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
+        .thenThrow(HttpError.badRequest);
 
-    verify(httpClient!.request(
-      url: url!,
-      method: 'post',
-      body: {
-        'email': params?.email,
-        'password': params?.password,
-      },
-    ));
-  });
+    final params = AuthenticationParams(
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    );
+    final future = sut!.auth(params);
 
-  test('Should call HttpClient with correct body params', () async {
-    await sut!.auth(params!);
-
-    verify(httpClient!.request(
-      url: url!,
-      method: 'post',
-      body: {
-        'email': params?.email,
-        'password': params?.password,
-      },
-    ));
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
