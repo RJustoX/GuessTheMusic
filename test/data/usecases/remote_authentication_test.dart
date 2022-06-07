e32b22fd2e2e4445df59d572693f8ba23c64c1ed
Rafael Justo <rafael.justoscherer@gmail.com>
2022-06-07 19:31:36 -0300

    :construction: começando testes erros 400 e 404

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -11,26 +11,26 @@ import 'package:guess_the_music/domain/usecases/authentication.dart';
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  RemoteAuthentication? sut;
-  String? url;
-  HttpClientSpy? httpClient;
+  late RemoteAuthentication sut;
+  late String url;
+  late HttpClientSpy httpClient;
+  late AuthenticationParams params;
 
   setUp(() {
     httpClient = HttpClientSpy();
     url = faker.internet.httpUrl();
-    sut = RemoteAuthentication(httpClient: httpClient!, url: url!);
-  });
-
-  test('Should call HttpClient with correct URL, method and params', () async {
-    final params = AuthenticationParams(
+    sut = RemoteAuthentication(httpClient: httpClient, url: url);
+    params = AuthenticationParams(
       email: faker.internet.email(),
       password: faker.internet.password(),
     );
+  });
 
-    await sut!.auth(params);
+  test('Should call HttpClient with correct URL, method and params', () async {
+    await sut.auth(params);
 
-    verify(httpClient!.request(
-      url: url!,
+    verify(httpClient.request(
+      url: url,
       body: {
         'email': params.email,
         'password': params.password,
@@ -39,15 +39,21 @@ void main() {
   });
 
   test('Should throw an unexpected exception if httpClient returns 400', () async {
-    when(httpClient!
-            .request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
+    when(httpClient.request(
+            url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
         .thenThrow(HttpError.badRequest);
 
-    final params = AuthenticationParams(
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    );
-    final future = sut!.auth(params);
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw an unexpected exception if httpClient returns 404', () async {
+    when(httpClient.request(
+            url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
+        .thenThrow(HttpError.notFound);
+
+    final future = sut.auth(params);
 
     expect(future, throwsA(DomainError.unexpected));
   });
